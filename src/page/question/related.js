18b507025fac3e011642fbd46d0aba4667d5d219
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2019-02-12 16:52:31 -0500

    update

--- src/page/question/related.js
+++ src/page/question/related.js
@@ -4,78 +4,67 @@ import {
   MDBBtn,
   MDBRow,
   MDBCol,
+  MDBIcon,
 } from 'mdbreact';
-import {JobCardSquare} from '../../general-component/job-card-square';
+
+const basicFont = {
+  fontFamily: 'IBM Plex Sans',
+  fontStyle: 'normal',
+  fontWeight: '600',
+  lineHeight: 'normal',
+}
 
 class RelatedPosition extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   contentStyle: {
-    //     height: '50px',
-    //     overflow: 'hidden',
-    //     whiteSpace: 'nowrap',
-    //     OTextOverflow: 'ellipsis',
-    //     TextOverflow: 'ellipsis'
-    //   },
-    //   backend: null
-    // }
-    this.handleInputClick = this.handleInputClick.bind(this);
+    this.state = {
+      backend: null,
+      isCollapsed: true,
+      stickyRow: {
+        background: '#FFFFFF',
+        // position: 'fixed',
+        // bottom: '0px',
+      },
+    }
+    this.handleSpanClick = this.handleSpanClick.bind(this);
     this.text = RelatedPosition.i18n[languageHelper()];
   }
 
-  handleInputClick() {
-
-    if (Object.keys(this.state.contentStyle).length === 0) {
-
+  handleSpanClick() {
+    let isCollapsed = !this.state.isCollapsed
+    if (isCollapsed) {
+      // this.divNow.style.height='100px'
       this.setState({
-        contentStyle: {
-          height: '50px',
-          overflow: 'hidden',
-          whiteSpace: 'nowrap',
-          OTextOverflow: 'ellipsis',
-          TextOverflow: 'ellipsis'
-        }
+        stickyRow: {
+          background: '#FFFFFF',
+        },
+        isCollapsed: true
       });
     } else {
+
+      // this.divNow.style.height='400px'
       this.setState({
-        contentStyle: {}
+        stickyRow: {
+          background: '#FFFFFF',
+          position: 'sticky',
+          bottom: '0px'
+        },
+        isCollapsed: false
       });
     }
+
   }
 
   componentWillMount() {
     let mockData =
       {
         id: 0,
-        name: 'New Frontier Data',
-        relatedWork: [
-          {
-            id1: {
-              content: '123'
-            }
-          },
-          {
-            id2: {
-              content: '123'
-            }
-          },
-          {
-            id3: {
-              content: '123'
-            }
-          },
-          {
-            id4: {
-              content: '123'
-            }
-          },
-          {
-            id5: {
-              content: '123'
-            }
-          },
-        ],
+        title: 'YouthChina是怎样的一个公司',
+        short: '关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式...',
+        long: '关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式关于这是一家什么公司，我想没有人比我更有发言权了，这要从远古时代开始讲起，记得那是一个平凡却又不平凡的下午，那天的夕阳很美，美的就像那个什么，对，就像那个什么。如果有其他问题，欢迎向我提问，反正你也没有我联系方式',
+        user: '齐昊',
+        agree: '',
+        disagree: '',
         status: {
           code: 2000
         }
@@ -84,24 +73,64 @@ class RelatedPosition extends React.Component {
       return {backend: mockData};
     });
   }
-
+  componentDidMount() {
+    window.addEventListener('scroll', this.orderScroll.bind(this));
+  }
+  orderScroll(){
+    console.log(1)  
+  }
+  
   render() {
     return (this.state.backend && this.state.backend.status && this.state.backend.status.code === 2000) ? (
-      <div style={{padding: '1rem'}}>
+      <div style={{padding: '30px'}}>
+        <h4 style={{color: '#3E4850', fontSize: '18px', ...basicFont}}>{this.state.backend.title}</h4>
         <div>
-          <h4><strong>{this.text.related}</strong></h4>
+          {this.state.isCollapsed ? (
+            <div>
+              <span style={{color: '#3E4850', fontSize: '14px', ...basicFont}}>{this.state.backend.user}</span>:
+              <span style={{color: '#62686C', fontSize: '14px', ...basicFont}}>  {this.state.backend.short}</span>
+              <span onClick={this.handleSpanClick}
+                    style={{color: '#175199', fontSize: '14px', ...basicFont}}>阅读全文</span><MDBIcon icon="angle-down"/>
+            </div>
+          ) : (
+            <div>
+              <span ref={(span) => this.scrollSpan = span} style={{color: '#3E4850', fontSize: '14px', ...basicFont}}>{this.state.backend.long}</span>
+
+            </div>
+          )}
+
+          <br/>
+          <MDBRow ref={(row) => this.scrollRow = row} style={this.state.stickyRow}>
+            <MDBBtn flat style={{padding: '5px 0', marginLeft: '15px'}}>
+              <MDBIcon style={{marginRight: '5px'}} far icon="star"/>关注问题
+            </MDBBtn>
+
+            <MDBBtn flat style={{padding: '5px 10px',}}>
+              <MDBIcon style={{marginRight: '5px'}} far icon="edit"/>写回答
+            </MDBBtn>
+            <MDBBtn flat style={{padding: '5px 10px',}}>
+              <MDBIcon style={{marginRight: '5px'}} icon="user-plus"/>邀请回答
+            </MDBBtn>
+            <MDBBtn flat style={{padding: '5px 10px',}}>
+              <MDBIcon style={{marginRight: '5px'}} far icon="comment"/>评论
+            </MDBBtn>
+            <MDBBtn flat style={{padding: '5px 10px',}}>
+              <MDBIcon style={{marginRight: '5px'}} icon="share"/>分享
+            </MDBBtn>
+            <MDBBtn flat style={{padding: '5px 10px',}}>
+              <MDBIcon style={{marginRight: '5px'}} icon="ban"/>
+              举报
+            </MDBBtn>
+            {this.state.isCollapsed ? null :
+              <MDBBtn onClick={this.handleSpanClick} flat
+                      style={{padding: '5px 10px', color: '#175199', fontSize: '14px', ...basicFont}}>
+                收起
+                <MDBIcon style={{marginRight: '5px'}} icon="arrow-up"/>
+              </MDBBtn>}
+          </MDBRow>
         </div>
-        <div style={this.state.contentStyle}>
-          {this.state.backend.relatedWork.map((item) => {
-            return (
-              <MDBCol size="4" key={item}>
-                <JobCardSquare></JobCardSquare>
-              </MDBCol>
-            );
-          })}
-        </div>
-        <br/>
-        <MDBBtn flat style={{width: '100%'}} onClick={this.handleInputClick}>See More</MDBBtn>
+
+
       </div>
     ) : null;
   }
