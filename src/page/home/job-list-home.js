6155163d8811b9185a34cf70a08db2d3b27b8e86
Yorick <772479358@qq.com>
2019-02-19 00:28:19 -0500

    update job-list-home component based on latest version

--- src/page/home/job-list-home.js
+++ src/page/home/job-list-home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import {
   MDBCol,
-  MDBRow
+  MDBRow,
+  MDBContainer
 } from 'mdbreact';
 
 import {JobCardSquare} from '../../general-component/job-card-square';
@@ -19,7 +20,7 @@ export class JobListHome extends React.Component {
   componentWillMount() {
     let mockData =
       {
-        searchResult: ['1', '1', '1', '1'],
+        searchResult: ['1', '1', '1', '1', '1', '1'],
         status: {
           code: 2000
         }
@@ -36,46 +37,55 @@ export class JobListHome extends React.Component {
           padding: '24px 16px'
         }}
       >
-        <div
-          style={{
-            marginBottom: '16px'
-          }}
-        >
-          <span style={{
-            fontSize: '24px'
-          }}>
-            {this.props.name}
-          </span>
-        </div>
-        <MDBRow>
-          {
-            this.state.backend.searchResult.map(
-              (item, index) =>
-                <MDBCol
-                  key={index}
-                >
-                  <JobCardSquare
-                    id={item}
-                  />
-                </MDBCol>
-            )
-          }
-        </MDBRow>
-        <div>
-          <div>
-            <a
-              href="https://www.google.com/"
-              style={{
-                marginTop: '8px',
-                display: 'inline-block',
-                fontWeight: '500',
-                color: '#1569e0'
-              }}
-            >
-              See More →
-            </a>
+        <MDBContainer>
+          <div
+            style={{
+              marginBottom: '16px'
+            }}
+          >
+            <span style={{
+              fontSize: '18px',
+              color: '#454F69'
+            }}>
+              {this.props.name}
+            </span>
           </div>
-        </div>
+
+          <MDBRow center>
+            {
+              this.state.backend.searchResult.map(
+                (item, index) =>
+                  <MDBCol
+                    key={index}
+                    className="my-3 px-4 py-1"
+                    size="12"
+                    md="6"
+                    xl="4"
+                  >
+                    <JobCardSquare
+                      id={item}
+                    />
+                  </MDBCol>
+              )
+            }
+          </MDBRow>
+          <div>
+            <div>
+              <a
+                href="https://www.google.com/"
+                style={{
+                  fontSize: '16px',
+                  marginTop: '8px',
+                  display: 'inline-block',
+                  fontWeight: '500',
+                  color: '#454F69'
+                }}
+              >
+                显示全部
+              </a>
+            </div>
+          </div>
+        </MDBContainer>
       </div>
     ) : null;
   }
