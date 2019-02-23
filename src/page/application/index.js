0f7980c88d54eb5e4cfb1ee1ec525cf250754f88
guo <guo@guodeMBP.fios-router.home>
2019-02-23 13:31:48 -0500

    vv

--- src/page/application/index.js
+++ src/page/application/index.js
@@ -3,14 +3,11 @@ import {Redirect, Route, Switch} from 'react-router-dom';
 import {
   MDBCol,
   MDBRow,
-  MDBCard,
-  MDBCardBody,
-  MDBDataTable,
   MDBBtn,
 
 } from 'mdbreact';
 import {languageHelper} from "../../tool/language-helper";
-import {Header} from '../../general-component/header';
+import {Header, MDBNavItem} from '../../general-component/header';
 import {Footer} from "../../general-component/footer";
 import {ResumeTitle} from "../../general-component/resumeTitle";
 import {removeUrlSlashSuffix} from "../../tool/remove-url-slash-suffix";
@@ -22,11 +19,26 @@ export class Application extends React.Component {
     super(props)
     this.state = {
       isOpen: false,
+      hover: 0,
     };
     this.text = Application.i18n[languageHelper()];
     this.pip = 0;
   }
-
+  onClick(clickID){
+    this.setState( {
+      chosen:clickID
+    });
+  }
+  onMouseEnter(enterID){
+    this.setState( {
+      hover:enterID
+    });
+  }
+  onMouseLeave(leaveID){
+    this.setState( {
+      hover:leaveID
+    });
+  }
   
 
   render() {
@@ -43,28 +55,35 @@ export class Application extends React.Component {
 
             <div className="d-flex flex-row">
               <div>
-                <MDBRow>
-                  <MDBCol style={{textAlign: 'left'}}>
-                    <p>
-                      <MDBBtn flat href={`${this.props.match.url}/application`}>我的申请</MDBBtn>
-                    </p>
-                    <p>
-                      <MDBBtn  to="/home" flat>个人档案库</MDBBtn>
-                    </p>
-                    <p>
-                      <MDBBtn flat>我的简历</MDBBtn>
-                    </p>
-                    <p>
-                      <MDBBtn flat href={`${this.props.match.url}/collection`}>收 藏</MDBBtn>
-                    </p>
-                    <p>
-                      <MDBBtn flat>好友列表</MDBBtn>
-                    </p>
-                    <p>
-                      <MDBBtn flat>消 息</MDBBtn>
-                    </p>
-                  </MDBCol>
-                </MDBRow>
+                <p style={{textAlign:'left'}}>
+                  
+                  <p>
+                    <MDBBtn
+                      
+                      style={this.props.location.pathname.indexOf('/application/application') > -1 ? {fontWeight:'450',borderLeft: '4px solid #7C97B8',fontSize:'16px',background:'#F2F2F2'}:{fontWeight:'300',fontSize:'16px'}} 
+                      flat 
+                      href={`${this.props.match.url}/application` } 
+                    >我的申请</MDBBtn>
+                  </p>
+                  <p >
+                    <MDBBtn style={{fontWeight:'300',fontSize:'16px'}} flat>个人档案库</MDBBtn>
+                  </p>
+                  <p>
+                    <MDBBtn style={{fontWeight:'300',fontSize:'16px'}} flat>我的简历</MDBBtn>
+                  </p>
+                  <p>
+                    <MDBBtn
+                     
+                      style={this.props.location.pathname.indexOf('/collection/company')>-1||this.props.location.pathname.indexOf('/collection/job') > -1 ? {fontWeight:'450',borderLeft: '4px solid #7C97B8',fontSize:'16px',background:'#F2F2F2'}:{fontWeight:'300',fontSize:'16px'}}
+                      flat href={`${this.props.match.url}/collection`}>收 藏</MDBBtn>
+                  </p>
+                  <p>
+                    <MDBBtn style={{fontWeight:'300',fontSize:'16px'}} flat>好友列表</MDBBtn>
+                  </p>
+                  <p>
+                    <MDBBtn style={{fontWeight:'300',fontSize:'16px'}} flat>消 息</MDBBtn>
+                  </p>
+                </p>
               </div>
               <div className="flex-fill">
                 <Switch>
