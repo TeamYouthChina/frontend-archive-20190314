b8b1d1ed2e8cb0c08f6f72034b0ba11a2ff0b6b7
Zepei Xu <xuzepei19950617@gmail.com>
2019-02-28 14:52:48 -0500

    used prettier

--- src/page/OnlineApplication/Components/Resumes/resumes.js
+++ src/page/OnlineApplication/Components/Resumes/resumes.js
@@ -1,69 +1,67 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import classes from './resumes.module.css'
-import Resume from './Resume/resume';
+import classes from "./resumes.module.css";
+import Resume from "./Resume/resume";
 
-const resumes = (props) => {
+class resumes extends Component {
+  render() {
     let arrayOfResumes = Array();
-    arrayOfResumes = props.resumes.map((one,index)=>{
-        return (
-            <div key={index} className={classes.resumeWrapper}>
-                <Resume
-                    key={index}
-                    id={index}
-                    exportClicked={props.exportClicked} 
-                    deleteClicked={props.deleteClicked}
-                    icon={one.icon}
-                    name={one.name}
-                    language={one.language}
-                    lastEdit={one.lastEdit}
-                    selectHandler={props.selectHandler}
-                    selected={props.selected}
-                />
-            </div>
-            
-        );
+    arrayOfResumes = this.props.resumes.map((one, index) => {
+      return (
+        <div key={index} className={classes.resumeWrapper}>
+          <Resume
+            key={index}
+            id={index}
+            exportClicked={this.props.exportClicked}
+            deleteClicked={this.props.deleteClicked}
+            icon={one.icon}
+            name={one.name}
+            language={one.language}
+            lastEdit={one.lastEdit}
+            selectHandler={this.props.selectHandler}
+            selected={this.props.selected}
+          />
+        </div>
+      );
     });
-    
-    if(props.addingResume){
-        arrayOfResumes.push(
-            <div key={arrayOfResumes.length} className={[classes.resumeWrapper, classes.dotted].join(' ')}>
-                <div className={classes.centerButton}>
-                    <button 
-                        className={classes.halfButton}
-                        onClick={props.typeResumeHander}>
-                        Type Resume
-                    </button> 
-                    <button 
-                        className={classes.halfButton}
-                        onClick={props.uploadResuemHandler}>
-                        Upload From Device
-                    </button>
-                </div> 
-        </div>
-        )
-    } else {
-        arrayOfResumes.push(
-            <div key={arrayOfResumes.length} className={classes.resumeWrapper}>
-                <button key={arrayOfResumes.length} className={classes.addResume} onClick={props.addResumeClicked}>
-                    + Add Resume
-                </button> 
-            </div>  
-        );
-    }
-    
-    
 
-    return (
-        <div className={classes.resumes}>
-            {arrayOfResumes}
-            {/* <div key={arrayOfResumes.length} className={classes.resumeWrapper}>
-                <button key={arrayOfResumes.length} className={classes.addResume} onClick={props.addResumeClicked}>
-                    + Add Resume
-                </button> 
-            </div> */}
+    if (this.props.addingResume) {
+      arrayOfResumes.push(
+        <div
+          key={arrayOfResumes.length}
+          className={[classes.resumeWrapper, classes.dotted].join(" ")}
+        >
+          <div className={classes.centerButton}>
+            <button
+              className={classes.halfButton}
+              onClick={this.props.typeResumeHander}
+            >
+              Type Resume
+            </button>
+            <button
+              className={classes.halfButton}
+              onClick={this.props.uploadResuemHandler}
+            >
+              Upload From Device
+            </button>
+          </div>
         </div>
-    );
-};
+      );
+    } else {
+      arrayOfResumes.push(
+        <div key={arrayOfResumes.length} className={classes.resumeWrapper}>
+          <button
+            key={arrayOfResumes.length}
+            className={classes.addResume}
+            onClick={this.props.addResumeClicked}
+          >
+            + Add Resume
+          </button>
+        </div>
+      );
+    }
+    return <div className={classes.resumes}>{arrayOfResumes}</div>;
+  }
+}
 
 export default resumes;
