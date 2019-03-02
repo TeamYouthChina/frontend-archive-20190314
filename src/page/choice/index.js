7f4ab2b9681e655cd161dc0aeb3eadbc5a4b975d
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2019-03-02 16:08:35 -0500

    choice

--- src/page/choice/index.js
+++ src/page/choice/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
 
+import {
+  MDBRow,
+  MDBCol
+} from 'mdbreact';
+
 import {languageHelper} from '../../tool/language-helper';
 import {removeUrlSlashSuffix} from '../../tool/remove-url-slash-suffix';
+import {Header} from '../../general-component/header/header';
+import {Footer} from '../../general-component/footer';
+import {JobCardSquare} from '../../general-component/job-card-square/job-card-square';
 
 export class Choice extends React.Component {
   constructor(props) {
@@ -19,17 +27,129 @@ export class Choice extends React.Component {
     }
     return (
       <div>
-        
+        <Header/>
+        <div
+          className="row justify-content-center"
+          style={{
+            backgroundColor: 'rgb(49, 57, 77)',
+            padding: '2rem 0rem'
+          }}
+        >
+          <div className="col-md-12 col-lg-10 col-xl-8">
+            <div className="row">
+              <div className="col-2" style={{marginBottom: '1rem'}}>
+                <img src="https://s2.ax1x.com/2019/01/27/kuUMYq.jpg" width="100"/>
+              </div>
+              <div className="col-md-12 col-lg-10 col-xl-8">
+                <h2 style={{color: 'white'}}>欢迎来到 WeYouth，郭益豪。</h2>
+                <h5><a href="/profile" style={{color: 'white'}}>View Profile -></a></h5>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div
+          className="row justify-content-center"
+          style={{
+            backgroundColor: '#f7f7f7',
+            padding: '1rem 0rem'
+          }}
+        >
+          <div className="col-md-12 col-lg-10 col-xl-8">
+            <div style={{marginBottom: '1rem'}}>
+              <h2>根据专业为你选择的工作</h2>
+            </div>
+            <div className="row">
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+            </div>
+            <div>
+              <h6><a href="/search">查看更多 -></a></h6>
+            </div>
+          </div>
+        </div>
+        <div
+          className="row justify-content-center"
+          style={{
+            padding: '1rem 0rem'
+          }}
+        >
+          <div className="col-md-12 col-lg-10 col-xl-8">
+            <div style={{marginBottom: '1rem'}}>
+              <h2>根据你的兴趣为你选择的工作</h2>
+            </div>
+            <div className="row">
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+              <div
+                className="col-4"
+                style={{marginBottom: '1rem'}}
+              >
+                <JobCardSquare/>
+              </div>
+            </div>
+            <div>
+              <h6><a href="/search">查看更多 -></a></h6>
+            </div>
+          </div>
+        </div>
+        <Footer/>
       </div>
     );
   }
 }
 
 Choice.i18n = [
-  {
-    
-  },
-  {
-    
-  },
+  {},
+  {},
 ];
