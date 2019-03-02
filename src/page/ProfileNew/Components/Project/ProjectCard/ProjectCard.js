5010350833bfff8da9e843cb6d2e8b4ba4dd3e29
Zepei Xu <xuzepei19950617@gmail.com>
2019-03-02 18:41:39 -0500

    change profilenew views

--- src/page/ProfileNew/Components/Project/ProjectCard/ProjectCard.js
+++ src/page/ProfileNew/Components/Project/ProjectCard/ProjectCard.js
@@ -1,93 +1,155 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import classes from './ProjectCard.module.css';
-import Dropdown from '../../Dropdown/Dropdown';
+import classes from "./ProjectCard.module.css";
+import Dropdown from "../../Dropdown/Dropdown";
+import { languageHelper } from "../../../../../tool/language-helper";
 
+const translation = [
+  {
+    name: "名称",
+    begin: "开始日期",
+    end: "结束日期",
+    note: "说明",
+  },
+  {
+    name: "Name",
+    begin: "Begin",
+    end: "End",
+    note: "Note",
+  },
+];
+const text = translation[languageHelper()];
 
 class projectCard extends Component {
-  
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-        editing: this.props.data ? false : true,
-        proData: this.props.data ? {
+      editing: this.props.data ? false : true,
+      proData: this.props.data
+        ? {
             name: this.props.data.name,
             duration: {
-                begin: this.props.data.duration.begin,
-                end: this.props.data.duration.end
+              begin: this.props.data.duration.begin,
+              end: this.props.data.duration.end,
             },
-            note: this.props.data.note
-        } : {
-            name: '',
+            note: this.props.data.note,
+          }
+        : {
+            name: "",
             duration: {
-                begin: '',
-                end: ''   
+              begin: "",
+              end: "",
             },
-            note: ''
-        }
-    } 
+            note: "",
+          },
+    };
     this.nameRef = React.createRef();
     this.beginRef = React.createRef();
     this.endRef = React.createRef();
     this.noteRef = React.createRef();
+  }
 
-}
+  // this method only toggle 'editing'
+  editHandler = () => {
+    this.setState({ editing: true });
+  };
 
-    // this method only toggle 'editing'
-    editHandler=()=>{
-        this.setState({editing: true});
+  // tell parent the id of the current card
+  deleteHandler = () => {
+    this.props.deleteHandler(this.props.id);
+  };
+
+  saveHandler = () => {
+    this.setState({
+      ...this.state,
+      editing: false,
+    });
+    this.props.saveHandler(this.state.proData, this.props.id);
+  };
+
+  inputOnChange = () => {
+    this.setState({
+      ...this.state,
+      proData: {
+        name: this.nameRef.current.value,
+        duration: {
+          begin: this.beginRef.current.value,
+          end: this.endRef.current.value,
+        },
+        note: this.noteRef.current.value,
+      },
+    });
+  };
+
+  render() {
+    let toShow = (
+      <div className={classes.ProjectCard}>
+        <input
+          style={{ margin: "3px 0px" }}
+          disabled
+          type="text"
+          value={this.state.proData.name}
+        />
+        <div style={{ margin: "3px 0px" }} className={classes.Time}>
+          <p>
+            {this.state.proData.duration.begin} -{" "}
+            {this.state.proData.duration.end}
+          </p>
+        </div>
+        <input 
+                    style={{ margin: "15px 0px 3px 0px" }}
+                    disabled type="text" value={this.state.proData.note} />
+        <Dropdown delete={this.deleteHandler} edit={this.editHandler} />
+      </div>
+    );
+
+    if (this.state.editing) {
+      console.log(this.state.proData.duration.end);
+      toShow = (
+        <div className={classes.ProjectCard}>
+          <input
+          style={{ margin: "3px 0px" }}
+            type="text"
+            value={this.state.proData.name}
+            ref={this.nameRef}
+            placeholder={text.name}
+            onChange={this.inputOnChange}
+          />
+          <input
+          style={{ margin: "3px 0px" }}
+            type="text"
+            value={this.state.proData.duration.begin}
+            ref={this.beginRef}
+            placeholder={text.begin}
+            onChange={this.inputOnChange}
+          />
+          <input
+          style={{ margin: "3px 0px" }}
+            type="text"
+            value={this.state.proData.duration.end}
+            ref={this.endRef}
+            placeholder={text.end}
+            onChange={this.inputOnChange}
+          />
+          <input
+          style={{ margin: "3px 0px" }}
+            type="text"
+            value={this.state.proData.note}
+            ref={this.noteRef}
+            placeholder={text.note}
+            onChange={this.inputOnChange}
+          />
+          <Dropdown
+            delete={this.deleteHandler}
+            edit={this.editHandler}
+            editing
+            save={this.saveHandler}
+          />
+        </div>
+      );
     }
-
-    // tell parent the id of the current card
-    deleteHandler=()=>{
-        this.props.deleteHandler(this.props.id);
-    }
-
-    saveHandler=()=>{
-        this.setState({
-            editing: false,
-            proData: {
-                name: this.nameRef.current.value,
-                duration: {
-                    begin: this.beginRef.current.value,
-                    end: this.endRef.current.value,
-                },
-                note: this.noteRef.current.value
-            }
-        }, ()=>{
-            this.props.saveHandler(this.state.proData, this.props.id);     
-        });
-    }
-
-    render(){
-
-        let toShow =
-                <div className={classes.ProjectCard}>
-                        <input disabled type="text" defaultValue={this.state.proData.name} ref={this.nameRef}/>
-                        <div className={classes.Time}>
-                            <input disabled type="text" defaultValue={this.state.proData.duration.begin} ref={this.beginRef}/>
-                            <p> - </p>
-                            <input disabled type="text" defaultValue={this.state.proData.duration.end} ref={this.endRef}/>
-                        </div>
-                        <input disabled type="text" defaultValue={this.state.proData.note} ref={this.noteRef}/>
-                    <Dropdown delete={this.deleteHandler} edit={this.editHandler}/>
-                </div>;
-
-        if(this.state.editing){
-        toShow = 
-            <div className={classes.ProjectCard}>
-                <input type="text" defaultValue={this.state.proData.name} ref={this.nameRef}/>
-                <div className={classes.Time}>
-                    <input type="text" defaultValue={this.state.proData.duration.begin} ref={this.beginRef}/>
-                    <p> - </p>
-                    <input type="text" defaultValue={this.state.proData.duration.end} ref={this.endRef}/>
-                </div>
-                <input type="text" defaultValue={this.state.proData.note} ref={this.noteRef}/>
-                <Dropdown delete={this.deleteHandler} edit={this.editHandler} editing save={this.saveHandler}/>
-            </div>
-        }
-        return( toShow )
-    }
+    return toShow;
+  }
 }
 
 export default projectCard;
--- src/page/ProfileNew/Components/Skill/SkillCard/SkillCard.js
+++ src/page/ProfileNew/Components/Skill/SkillCard/SkillCard.js
@@ -1,68 +1,100 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import classes from './SkillCard.module.css';
-import SkillIcon from '../../../../assets/javascript.png';
-import Dropdown from '../../Dropdown/Dropdown';
+import classes from "./SkillCard.module.css";
+import SkillIcon from "../../../../assets/javascript.png";
+import Dropdown from "../../Dropdown/Dropdown";
+import { languageHelper } from "../../../../../tool/language-helper";
 
-class skillCard extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            editing: this.props.data ? false : true,
-            skillData: this.props.data ? {
-                name: this.props.data.name,
-            } : {
-                name: ''
-            }
-        } 
-        this.nameRef = React.createRef();
+const translation = [
+  {
+    name: "名称",
+  },
+  {
+    name: "Name",
+  },
+];
+const text = translation[languageHelper()];
+class skillCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: this.props.data ? false : true,
+      skillData: this.props.data
+        ? {
+            name: this.props.data.name,
+          }
+        : {
+            name: "",
+          },
+    };
+    this.nameRef = React.createRef();
+  }
 
+  // this method only toggle 'editing'
+  editHandler = () => {
+    this.setState({ editing: true });
+  };
+
+  // tell parent the id of the current card
+  deleteHandler = () => {
+    this.props.deleteHandler(this.props.id);
+  };
+
+  saveHandler = () => {
+    this.setState({
+      ...this.state,
+      editing: false,
+    });
+    this.props.saveHandler(this.state.skillData, this.props.id);
+  };
+
+  inputOnChange = () => {
+    this.setState({
+      ...this.state,
+      skillData: {
+        name: this.nameRef.current.value,
+      },
+    });
+  };
+
+  render() {
+    let toShow = (
+      <div className={classes.SkillCard}>
+        <img src={SkillIcon} alt="no img" />
+        <div className={classes.SkillInfo}>
+          <div style={{ margin: "3px 0px" }} className={classes.oneP}>
+            <p>{this.state.skillData.name}</p>
+          </div>
+        </div>
+        <Dropdown delete={this.deleteHandler} edit={this.editHandler} />
+      </div>
+    );
+
+    if (this.state.editing) {
+      toShow = (
+        <div className={classes.SkillCard}>
+          <img src={SkillIcon} alt="no img" />
+          <div className={classes.SkillInfo}>
+            <input
+              style={{ margin: "3px 0px" }}
+              type="text"
+              value={this.state.skillData.name}
+              ref={this.nameRef}
+              placeholder={text.name}
+              onChange={this.inputOnChange}
+            />
+          </div>
+          <Dropdown
+            delete={this.deleteHandler}
+            edit={this.editHandler}
+            editing
+            save={this.saveHandler}
+          />
+        </div>
+      );
     }
-
-    // this method only toggle 'editing'
-    editHandler=()=>{
-        this.setState({editing: true});
-    }
-
-    // tell parent the id of the current card
-    deleteHandler=()=>{
-        this.props.deleteHandler(this.props.id);
-    }
-
-    saveHandler=()=>{
-        this.setState({
-            editing: false,
-            skillData: {
-                name: this.nameRef.current.value
-            }
-        }, ()=>{
-            this.props.saveHandler(this.state.skillData, this.props.id);     
-        });
-    }
-
-
-    render(){
-        let toShow =
-            <div className={classes.SkillCard}>
-                <img src={SkillIcon} alt="no img"></img>
-                <div className={classes.SkillInfo}>
-                    <input disabled type="text" defaultValue={this.state.skillData.name} ref={this.nameRef}/>
-                </div>                
-                <Dropdown delete={this.deleteHandler} edit={this.editHandler}/>
-            </div>;
-        
-        if(this.state.editing){
-            toShow = 
-                <div className={classes.SkillCard}>
-                    <img src={SkillIcon} alt="no img"></img>
-                    <div className={classes.SkillInfo}>
-                        <input type="text" defaultValue={this.state.skillData.name} ref={this.nameRef}/>
-                    </div>
-                    <Dropdown delete={this.deleteHandler} edit={this.editHandler} editing save={this.saveHandler}/>
-                </div>;
-        }
-        return(toShow)
-    }
-};
+    return toShow;
+  }
+}
 
 export default skillCard;
