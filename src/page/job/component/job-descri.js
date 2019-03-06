a3592099b03777d1be5a7bd2293e58e756a19775
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2019-03-06 13:37:25 -0500

    commit

--- src/page/job/component/job-descri.js
+++ src/page/job/component/job-descri.js
@@ -26,11 +26,11 @@ export class JobDescri extends React.Component {
   async componentDidMount() {
     if (this.props.id) {
       this.setState({
-        backend: await getAsync(`/jobs/${this.props.id}`)
+        backend: await getAsync(`/jobs/${this.props.id}`, true)
       });
     } else {
       this.setState({
-        backend: await getAsync(`/jobs/1`)
+        backend: await getAsync(`/jobs/1`, true)
       });
     }
   }
